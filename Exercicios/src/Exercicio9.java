2c918e14ff04921bdeeee238d17a9866c7064444
gaby.santos <gaby.31678@gmail.com>
2023-09-01 09:50:07 -0300

    faltou exr9

--- Exercicios/src/Exercicio9.java
+++ Exercicios/src/Exercicio9.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Exercicio9 {
 
@@ -8,7 +9,45 @@ public class Exercicio9 {
 		   O programa deve solicitar o cargo e o salário de cada funcionário e mostrar o salário atual, 
 		   qual foi o aumento e qual o valor do novo salário.
 		 */
-
+		Exercicio9();
 	}
 
+	public static Exercicio9 () {
+
+		// Solicita o cargo e o salário do funcionário
+
+		System.out.print("Digite o cargo do funcionário (Gerente, Supervisor, Operador, Outro): ");
+		String cargo = new Scanner(System.in).next();
+		System.out.print("Digite o salário do funcionário: ");
+		double salario = new Scanner(System.in).nextDouble();
+
+		// Definir as taxas de aumento com base no cargo
+		double taxaAumento;
+		switch (cargo.toLowerCase()) {
+			case "gerente":
+				taxaAumento = 0.05;
+				break;
+			case "supervisor":
+				taxaAumento = 0.08;
+				break;
+			case "operador":
+				taxaAumento = 0.09;
+				break;
+			default:
+				taxaAumento = 0.10;
+				break;
+		}
+
+		// Calcular o aumento e o novo salário
+		double aumento = salario * taxaAumento;
+		double novoSalario = salario + aumento;
+
+		// Mostrar os resultados
+		System.out.printf("Salário atual: R$%.2f%n", salario);
+		System.out.printf("Aumento: R$%.2f%n", aumento);
+		System.out.printf("Novo salário: R$%.2f%n", novoSalario);
+
+	}
 }
+
+
